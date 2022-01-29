af286d259919e68776b0dbdb7a2813ac48f4ef5e
Carlos Rojas <carlosrb206@gmail.com>
2022-01-29 13:56:29 -0500

    prompt and logic

---
+++ index.js
@@ -0,0 +1,218 @@
+const inquirer = require('inquirer');
+const db = require('./db/connection');
+const { getAllEmployee, addEmployee } = require('./lib/employee');
+const { getDepartments, addDepartment } = require('./lib/departments');
+const { getRoles, addRole, updateEmployeeRole } = require('./lib/roles')
+
+
+init = () => {
+  inquirer
+  .prompt([
+    {
+      type: 'list',
+      name: 'action',
+      message: 'What would you like to do?',
+      choices: [
+        'View All Departments', 
+        'View All Roles',
+        'View All Employees',
+        'Add a Department',
+        'Add a Role',
+        'Add an Employee',
+        'Update Employee Role'
+      ] 
+    }
+  ])
+  .then(answer => {
+    switch (answer.action) {
+      case 'View All Departments':
+        getDepartments()
+        .then(departments => console.table(departments))
+        .then(() => askAgain())
+        .catch((err) => console.log(err));
+      break;
+
+      case 'View All Roles':
+        getRoles()
+        .then(roles => console.table(roles))
+        .then(() => askAgain())
+        .catch((err) => console.log(err));
+      break;
+
+      case 'View All Employees':
+        getAllEmployee()
+        .then(employees => console.table(employees))
+        .then(() => askAgain())
+        .catch((err) => console.log(err));
+      break;
+
+      case 'Add a Department':
+        inquirer
+        .prompt([
+          {
+            type: 'input',
+            name: 'department',
+            message: 'What department would you like to add?'
+          }
+        ])
+        .then(answer => addDepartment(answer.department))
+        .then(() => askAgain())
+        .catch((err) => console.log(err));
+      break;
+
+      case 'Add a Role':
+        let departments = [];
+        getDepartments() 
+        .then(results => {
+          for (i = 0; i < results.length; i++) {
+              departments.push(results[i].name);
+          }
+        })
+        .then(() => {
+          inquirer
+          .prompt([
+            {
+              type: 'input',
+              name: 'role',
+              message: 'What title do you want to add?'
+            },
+            {
+              type: 'number',
+              name: 'salary',
+              message: 'What is the salary?'
+            },
+            {
+              type: 'list',
+              name: 'department',
+              message: 'Please select a department for this new role.',
+              choices: departments
+            }
+          ])
+          .then(answers => {
+              addRole(answers.role, answers.salary, answers.department)
+                  .then(() => askAgain());
+          })
+          .catch((err) => console.log(err));
+        });
+      break;
+          
+      case 'Add an Employee':
+        let adderEmployees = [];
+        let adderRoles = [];
+          
+        getAllEmployee()
+          .then(results => {
+            for (i = 0; i < results.length; i++) {
+            adderEmployees.push(results[i].first_name + ' ' + results[i].last_name);
+            }
+          })
+          .then(() => {
+            getRoles()
+              .then(results => {
+                for (i = 0; i < results.length; i++) {
+                    adderRoles.push(results[i].title);
+                }
+              });
+          })
+          .then(() => {
+            inquirer
+            .prompt([
+              {
+                type: 'input',
+                name: 'first_name',
+                message: "What is the new employee's first name?"
+              },
+              {
+                type: 'input',
+                name: 'last_name',
+                message: "What is the new employee's last name?"
+              },
+              {
+                type: 'list',
+                name: 'title',
+                message: "What is the new employee's title?",
+                choices: adderRoles
+              },
+              {
+                type: 'list',
+                name: 'manager',
+                message: "Who is the new employee's manager?",
+                choices: adderEmployees
+              }
+            ])
+            .then(answers => {
+              addEmployee(answers.first_name, answers.last_name, answers.title, answers.manager)
+            .then(() => {askAgain()})
+            .catch((err) => console.log(err));
+            });
+          });
+      break;
+
+      case 'Update Employee Role':
+        let updateEmployees = [];
+        let updateRoles = [];
+          
+        getEmployees()
+        .then(results => {
+          for (i = 0; i < results.length; i++) {
+            updateEmployees.push(results[i].first_name + ' ' + results[i].last_name);
+          }
+        })
+        .then(() => {
+          getRoles()
+            .then(results => {
+              for (i = 0; i < results.length; i++) {
+                updateRoles.push(results[i].title);
+              }
+            })
+            .then(() => {
+              inquirer
+              .prompt([
+                {
+                  type: 'list',
+                  name: 'employee',
+                  message: 'Please select an employee to update.',
+                  choices: updateEmployees
+                },
+                {
+                  type: 'list',
+                  name: 'role',
+                  message: 'Please select their new role.',
+                  choices: updateRoles
+                }
+              ])
+              .then(answers => {
+                updateEmployeeRole(answers.employee, answers.role)
+                .then(() => askAgain())
+                .catch((err) => console.log(err))
+                });
+
+            });
+        });
+    break;
+              
+  default:
+    break;
+    }
+  });
+}
+
+function askAgain() {
+  inquirer
+      .prompt([
+          {
+              type: 'list',
+              name: 'askAgain',
+              message: 'Would you like to perform another task?',
+              choices: ['Yes', 'No']
+          }
+      ]).then(answer => {
+          if (answer.askAgain === 'Yes') {
+              init();
+          } else {
+              db.end()
+          }
+  });
+}
+
+init();
